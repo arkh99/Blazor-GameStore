0b3bdda8b37e4ac17313d541ed23f34ac67c7ce2
Alireza Khoshnami <124736009+arkh99@users.noreply.github.com>
2024-07-11 14:32:11 -0400

    Add files via upload

---
+++ GameStore/GameStore.Frontend/Models/GameSummery.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.Frontend.Models;
+
+public class GameSummery
+{
+    public int id { get; set; }
+    public string? Name { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly ReleaseDate { get; set; } 
+}
