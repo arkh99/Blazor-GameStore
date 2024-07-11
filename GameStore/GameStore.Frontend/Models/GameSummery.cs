aeed56f48696d5753de4b2599adf2bfca88f736d
alireza <arkh919@gmail.com>
2024-07-11 15:45:54 -0400

    implementing gamesummery class and use it in html

--- GameStore/GameStore.Frontend/Models/GameSummery.cs
+++ GameStore/GameStore.Frontend/Models/GameSummery.cs
@@ -2,7 +2,7 @@
 
 public class GameSummery
 {
-    public int id { get; set; }
+    public int Id { get; set; }
     public string? Name { get; set; }
     public string? Genre { get; set; }
     public decimal Price { get; set; }
